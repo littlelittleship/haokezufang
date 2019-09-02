ea9a3f96712917731380f4677249767bc1109fc4
yang <1303733687@qq.com>
2019-09-02 15:07:59 +0800

    06-租房小组和最新资讯

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -20,15 +20,32 @@ class index extends Component {
   state = {
     swiperList: [],
     imgHeight: 176,
-    navList:[]
+    navList: [{ id: 0, title: '整租', img: nav1 }, { id: 1, title: '合租', img: nav2 }, { id: 2, title: '地图找房', img: nav3 }, { id: 3, title: '去出租', img: nav4 }],
+    //租房小组
+    homeGroups:[],
+    news:[]
   }
   componentDidMount() {
     
     axios.get("/home/swiper")
       .then(res => {
         // this.setState({ swiperList: res.data.body });
-        console.log(REACT_APP_API_URL);
+        // console.log(REACT_APP_API_URL);
         this.setState({ swiperList: res.body });
+      })
+
+    // 租房小组
+    axios.get('/home/groups')
+    .then(res=>{
+      console.log(res)
+      this.setState({ homeGroups: res.body });
+    })
+
+    //资讯
+    axios.get("/home/news")
+      .then(res => {
+        this.setState({ news: res.body });
+        console.log(res);
       })
   }
 
@@ -61,30 +78,69 @@ class index extends Component {
               </a>
             ))}
           </Carousel>}
-         
         </div>
         {/* 轮播图 结束 */}
+
         {/* 导航部分开始 */}
         <div className={style.home_nav}>
-          <div className={style.nav_item}>
-            <img src={nav1} alt=""/>
-            <p>整租</p>
-          </div>
-          <div className={style.nav_item}>
-            <img src={nav2} alt=""/>
-            <p>整租</p>
-          </div>
-          <div className={style.nav_item}>
-            <img src={nav3} alt=""/>
-            <p>整租</p>
-          </div>
-          <div className={style.nav_item}>
-            <img src={nav4} alt=""/>
-            <p>整租</p>
-          </div>
+          {
+            this.state.navList.map( v=>
+            <div key={v.id} className={style.nav_item}>
+              <img src={v.img} alt=""/>
+              <p>{v.title}</p>
+            </div>
+            )
+          }
+          
         </div>
         {/* 导航部分结束 */}
 
+        {/* 租房小组开始 */}
+        <div className={style.homeGroups}>
+          <div className={style.homeGroups_title}>
+            <span>租房小组</span>
+            <span>更多</span>
+          </div>
+          <div className={style.homeGroups_cont}>
+
+            {
+              this.state.homeGroups.map(v=>
+                <div key={v.id} className={style.cont_item}>
+                  <div className={style.item_desc}>
+                    <p>{v.title}</p>
+                    <p>{v.desc}</p>
+                  </div>
+                  <div className={style.item_img}>
+                    <img src={REACT_APP_API_URL + v.imgSrc} alt=""/>
+                  </div>
+                </div>
+              )
+            }
+          </div>
+        </div>
+        {/* 租房小组结束 */}
+
+        {/* 最新资讯开始 */}
+        <div className={style.home_news}>
+          <div className={style.home_news_title}>最新资讯</div>
+          <div className={style.home_news_content}>
+            {this.state.news.map(v =>
+              <div key={v.id} className={style.news_item}>
+                <div className={style.news_item_img}>
+                  <img src={REACT_APP_API_URL + v.imgSrc} alt="" />
+                </div>
+                <div className={style.news_item_info}>
+                  <div className={style.news_item_name}>{v.title}</div>
+                  <div className={style.news_item_des}>
+                    <span className={style.news_item_from} >{v.from}</span>
+                    <span className={style.news_item_date} >{v.date}</span>
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+        {/* 最新资讯结束 */}
       </div>
     );
   }
